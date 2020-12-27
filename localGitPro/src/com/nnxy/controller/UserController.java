c948f8b54c2f91a1d070117b833c5efb8df4261a
lc-b <74576010+lc-b@users.noreply.github.com>
2020-12-27 20:12:43 +0800

    Update UserController.java

--- localGitPro/src/com/nnxy/controller/UserController.java
+++ localGitPro/src/com/nnxy/controller/UserController.java
@@ -1,5 +1,13 @@
 package com.nnxy.controller;
 
 public class UserController {
+  [branch "master"] 
+	remote = origin 
+	merge = refs/heads/master
+[remote "origin"] 
+	url = https://github.com/lc-b/git_project.git 
+	fetch = +refs/heads/*:refs/remotes/origin/* 
+	push = refs/heads/master:refs/heads/master
+
 
 }
