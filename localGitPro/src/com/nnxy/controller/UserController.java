b572a90fb9cb5731bb250f619b7d158cdef4f5eb
lc-b <74576010+lc-b@users.noreply.github.com>
2020-12-27 20:14:25 +0800

    Update UserController.java

--- localGitPro/src/com/nnxy/controller/UserController.java
+++ localGitPro/src/com/nnxy/controller/UserController.java
@@ -1,6 +1,21 @@
 package com.nnxy.controller;
 
 public class UserController {
+	
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	
+	public void StudentModel(int id,String username,String password,int sex,String love) {
+	
+	   this.id = id;
+	   this.username = username;
+	   this.password = password;
+	   this.sex = sex;
+	   this.love = love;
+		
   [branch "master"] 
 	remote = origin 
 	merge = refs/heads/master
